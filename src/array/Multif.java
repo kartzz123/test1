b1f4bdb5b412a1928b865d42c94e55893a6cfb85
karthick <kartzz@gmail.com>
2018-08-20 13:48:04 +0530

    first commit

---
+++ src/array/Multif.java
@@ -0,0 +1,47 @@
+package array;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Multif {
+
+	public static void main(String[] args) {
+	
+		WebDriver driver= new InternetExplorerDriver();
+		driver.get("http://newtours.demoaut.com/");
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.findElement(By.className("mousename")).click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		;
+
+	}
+
+}
